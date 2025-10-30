764df32384ac0578d4949292b67f658c698cfca0
Micah <martinstefanovic043@gmail.com>
2025-10-30 07:26:28 +0100

    added commands

--- main.py
+++ main.py
@@ -16,12 +16,14 @@ EXEMPT_USER_ID = 701156951798841364 # use your own id etc.
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
+# check messages
 @bot.event
 async def on_message(message):
     # Ignore messages from bots or pinned messages
     if message.author.bot or message.pinned:
         return
-        
+
+    # Ignore messages from exempted user
     if message.author.id == EXEMPT_USER_ID:
         return   
 
@@ -35,7 +37,8 @@ async def on_message(message):
             print(f"Failed to delete message: {e}")
 
     await bot.process_commands(message)
-    
+
+# check edited messages
 @bot.event
 async def on_message_edit(before, after):
     if after.author.bot or after.pinned:
