29ec054c829ceb70006af50b6be4dd92945021ad
Micah <martinstefanovic043@gmail.com>
2025-11-03 23:14:27 +0100

    made messages clearer

--- commands/exclusions.py
+++ commands/exclusions.py
@@ -16,9 +16,9 @@ class ExclusionCommands(commands.Cog):
         if user.id not in guild_data["users"]:
             guild_data["users"].append(user.id)
             save_all()
-            await interaction.response.send_message(f"{user.mention} excluded.", ephemeral=True)
+            await interaction.response.send_message(f"{user.mention} will now be excluded.", ephemeral=True)
         else:
-            await interaction.response.send_message(f"{user.mention} already excluded.", ephemeral=True)
+            await interaction.response.send_message(f"{user.mention} is already excluded.", ephemeral=True)
 
     # Unexclude a user
     @app_commands.command(name="unexcludeuser", description="Remove a user from exclusions.")
@@ -28,9 +28,9 @@ class ExclusionCommands(commands.Cog):
         if user.id in guild_data["users"]:
             guild_data["users"].remove(user.id)
             save_all()
-            await interaction.response.send_message(f"{user.mention} unexcluded.", ephemeral=True)
+            await interaction.response.send_message(f"{user.mention} will now be unexcluded.", ephemeral=True)
         else:
-            await interaction.response.send_message(f"{user.mention} not excluded.", ephemeral=True)
+            await interaction.response.send_message(f"{user.mention} is not excluded.", ephemeral=True)
 
     # Exclude a channel
     @app_commands.command(name="excludechannel", description="Exclude a channel from filtering.")
@@ -40,9 +40,9 @@ class ExclusionCommands(commands.Cog):
         if channel.id not in guild_data["channels"]:
             guild_data["channels"].append(channel.id)
             save_all()
-            await interaction.response.send_message(f"{channel.mention} excluded.", ephemeral=True)
+            await interaction.response.send_message(f"{channel.mention} will now be excluded.", ephemeral=True)
         else:
-            await interaction.response.send_message(f"{channel.mention} already excluded.", ephemeral=True)
+            await interaction.response.send_message(f"{channel.mention} is already excluded.", ephemeral=True)
 
     # Unexclude a channel
     @app_commands.command(name="unexcludechannel", description="Remove a channel from exclusions.")
@@ -52,9 +52,9 @@ class ExclusionCommands(commands.Cog):
         if channel.id in guild_data["channels"]:
             guild_data["channels"].remove(channel.id)
             save_all()
-            await interaction.response.send_message(f"{channel.mention} unexcluded.", ephemeral=True)
+            await interaction.response.send_message(f"{channel.mention} will now be unexcluded again.", ephemeral=True)
         else:
-            await interaction.response.send_message(f"{channel.mention} not excluded.", ephemeral=True)
+            await interaction.response.send_message(f"{channel.mention} is not excluded.", ephemeral=True)
 
     # Show exclusions
     @app_commands.command(name="showexclusions", description="Show all excluded users and channels.")
@@ -65,7 +65,6 @@ class ExclusionCommands(commands.Cog):
         channels = ", ".join(f"<#{c}>" for c in guild_data["channels"]) or "None"
         await interaction.response.send_message(
             f"**Excluded Users:** {users}\n**Excluded Channels:** {channels}",
-            ephemeral=True,
         )
 
 
