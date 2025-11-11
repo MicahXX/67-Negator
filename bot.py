94673e250ba76033d02eb9cea8933ace0f1d34d5
Micah <martinstefanovic043@gmail.com>
2025-11-11 23:46:36 +0100

    fixes

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 intents.messages = True
+intents.reactions = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -12,6 +13,7 @@ async def load_extensions():
         "events.on_ready",
         "events.on_message",
         "events.on_message_edit",
+        "events.on_reaction",
         "commands.exclusions",
     ]:
         await bot.load_extension(ext)
